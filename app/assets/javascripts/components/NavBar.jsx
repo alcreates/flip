6ca672e52ce632a7f07960baf1ff7a9f2a163c24
Alvaro Martinez <alvaromartinez@Alvaros-MBP-2.home>
2017-11-30 17:59:47 -0500

    added bootstrap

--- app/assets/javascripts/components/NavBar.jsx
+++ app/assets/javascripts/components/NavBar.jsx
@@ -3,7 +3,15 @@ class NavBar extends React.Component {
      render(){
          return (
              <div>
-                this is the nav bar  
+                 <nav className="navbar navbar-dark bg-faded">
+                    <div>
+                        <img className="logo" alt="Fund That Flip logo" src="https://cdn.fundthatflip.com/assets/final_navbar_logocs-ada796bc16394059988c2e9f5244bd7cd3b275b9bdd91c783a587d374bcd0374.svg"></img>            
+                    </div>
+
+                    <button type="button" className="btn btn-primary">Primary</button>
+
+                 </nav>
+
              </div>
          ); 
      }
