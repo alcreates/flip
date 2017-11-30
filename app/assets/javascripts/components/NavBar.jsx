b1fb6e0d9d96eacc7a5a8b0f3fc123d1b3ed542a
Alvaro Martinez <alvaromartinez@Alvaros-MBP-2.home>
2017-11-30 17:28:42 -0500

    fresh

---
+++ app/assets/javascripts/components/NavBar.jsx
@@ -0,0 +1,12 @@
+class NavBar extends React.Component {
+    
+     render(){
+         return (
+             <div>
+                this is the nav bar  
+             </div>
+         ); 
+     }
+     
+ }
+ 
