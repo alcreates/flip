3605fc2ac747419b243f65f7228d3c5feda2eedb
Alvaro Martinez <alvaromartinez@Alvaros-MBP-2.home>
2017-12-04 10:57:49 -0500

    he

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 # config/application.rb
-config.assets.initialize_on_precompile = false
+
 
 
 
@@ -15,6 +15,8 @@ module Project
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+
+    config.assets.initialize_on_precompile = false
 
     
 
